64605330200ab00c81ba23556a33c76bb215143a
Nikola Adamus <nikola.adamus@gmail.com>
2017-02-20 18:18:34 +0100

    delete alert function, add red rectangle to canvas

--- canvas.js
+++ canvas.js
@@ -1,1 +1,10 @@
-alert("Elo elo");
+var WIDTH = 500, HEIGHT = 500;
+
+function draw() {
+    var canvas = document.querySelector("#canvas");
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+}
+
+draw();
