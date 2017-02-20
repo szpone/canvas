a4471c117b4d19e5401c5997d56ae6439c1c8201
Nikola Adamus <nikola.adamus@gmail.com>
2017-02-20 20:08:15 +0100

    draw lines

--- canvas.js
+++ canvas.js
@@ -1,10 +1,15 @@
 var WIDTH = 500, HEIGHT = 500;
+var canvas = document.querySelector("#canvas");
+var ctx = canvas.getContext('2d');
+
+function fillCanvas(color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+}
 
 function draw() {
-    var canvas = document.querySelector("#canvas");
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    fillCanvas('rgb(200, 0, 0)');
+
 }
 
 function gradientColor(x) {
@@ -12,10 +17,8 @@ function gradientColor(x) {
 }
 
 function drawGradient() {
-    var canvas = document.querySelector("#canvas");
-    var ctx = canvas.getContext('2d');
     ctx.fillStyle = 'rgb(255, 255, 255)';
-    for (x = 0; x < WIDTH ; x++) {
+    for (var x = 0; x < WIDTH ; x++) {
         var col = gradientColor(x);
         ctx.fillStyle = 'rgb(' + col + ', ' + col + ', ' + col + ')';
         ctx.fillRect(x, 0, 1, HEIGHT);
@@ -23,9 +26,6 @@ function drawGradient() {
 }
 
 function drawAnimatedRect() {
-    var canvas = document.querySelector("#canvas");
-    var ctx = canvas.getContext('2d');
-
     var fps = 30;
     var intervalId = setInterval(drawFrame, 1000 / fps);
     // clearInterval(intervalId);
@@ -36,19 +36,38 @@ function drawAnimatedRect() {
     var h = 50;
 
     function drawFrame() {
-        var canvas = document.querySelector("#canvas");
-        var ctx = canvas.getContext('2d');
         x += 5;
         y += 5;
         if (x >= WIDTH - w && y >= HEIGHT - h) {
             clearInterval(intervalId);
         }
-
-        ctx.fillStyle = 'rgb(255, 255, 255)';
-        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        fillCanvas('rgb(255, 255, 255)');
         ctx.fillStyle = 'rgb(128, 128, 128)';
         ctx.fillRect(x, y, w, h);
     }
 }
 
+function drawFireworks() {
+    var xm = 0.5 * WIDTH
+    var ym = 0.5 * HEIGHT
+    var sparks = [
+        { x1: xm, y1: ym, x2: xm + 50, y2: ym + 50 },
+        { x1: xm, y1: ym, x2: xm - 50, y2: ym + 50 },
+        { x1: xm, y1: ym, x2: xm, y2: ym + 50},
+        { x1: xm, y1: ym, x2: xm - 50, y2: ym},
+        { x1: xm, y1: ym, x2: xm + 50, y2: ym},
+        { x1: xm, y1: ym, x2: xm + 50, y2: ym - 50 },
+        { x1: xm, y1: ym, x2: xm - 50, y2: ym - 50 },
+        { x1: xm, y1: ym, x2: xm , y2: ym - 50 },
+    ];
 
+    fillCanvas('rgb(0, 0, 0)');
+    ctx.strokeStyle = 'rgb(255, 255, 0)';
+    for (var i = 0 ; i < sparks.length ; i++) {
+        ctx.beginPath();
+        ctx.moveTo(0.5 * WIDTH, 0.5 * HEIGHT);
+        ctx.lineTo(sparks[i].x2, sparks[i].y2)
+        ctx.stroke();
+    }
+
+}
